e546a881d1f4fe30a64f04eb3bfcacaed55f9516
DRicciS <dricci@uandes.cl>
2025-04-11 16:31:23 -0400

    dbases, migrations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,73 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Message.delete_all
+Chat.delete_all
+User.delete_all
+
+emails = ['juanma@mail.com',  'richard@hello.cl',
+    'dog@whyImdoingthis.io',
+    'class22@Ihavetostudyfinance.it',
+    'saem@a.com',
+    'alice@ia.org',
+    'bob@nik.cl',
+    'carla@gob.com',
+    'julianpin@colombia.co',
+    'aaasadasd@enel.it']
+
+first_names = [
+    'Juan',
+    'Ricardo',
+    'Dog',
+    'Migel',
+    'Saem',
+    'Alicia',
+    'Bob',
+    'Carla',
+    'Julian',
+    'Arroz']
+
+last_names = [
+    'Pere',
+    'Rodri',
+    'Alonso',
+    'Luca',
+    'SAd',
+    'Smith',
+    'Loresdsadsada',
+    'Ramos',
+    'Pineda',
+    'Enel']
+
+emails.each_with_index do |email, i|
+    User.create!(
+      email:      email,
+      first_name: first_names[i],
+      last_name:  last_names[i]
+    )
+  end
+users = User.all  
+#Hay una forma ma rapida en vdd
+
+10.times do
+    sender   = users.sample
+    receiver = (users - [sender]).sample
+  
+    Chat.create!(
+      sender_id:   sender.id,
+      receiver_id: receiver.id
+    )
+  end
+  
+chats = Chat.all
+
+10.times do |i|
+    chat   = chats.sample
+    author = [chat.sender_id, chat.receiver_id].map { |uid| User.find(uid) }.sample
+  
+    Message.create!(
+      chat_id: chat.id,
+      user_id: author.id,
+      body:    "Mensage nro ##{i + 1}"
+    )
+  end
